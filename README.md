5924fb5f49503be1cf2ef382b634db6e416d2c00
崔晓欢 <cuixiaohuan@nash.work>
2018-01-02 15:13:50 +0800

    button

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Candy
 
+### 编译项目
+    | 执行 compaile.sh文件，编译结果到fepack-temp
+
 Candy 是一款极简单的基于 Vue2.x 的 移动 端的 UI 组件库。
+
---
+++ compile.sh
@@ -0,0 +1,10 @@
+echo remove last build
+rm -rf ./fepack-temp
+
+echo build
+fepack release qa
+
+echo change uploader aurora dir
+sed -i -e 's/="\/candy/="../g' ./fepack-temp/candy/tests/button.html
+
+echo done
