5d00e8cda843ffabf13489f047514374ed6558ae
崔晓欢 <cuixiaohuan@nash.work>
2018-01-03 10:55:32 +0800

    backup

--- src/common/_tools.js
+++ src/common/_tools.js
@@ -1,71 +1,4 @@
-function isArray (o){
-  return Object.prototype.toString.call(o) === '[object Array]'
-}
-function isObject (o) {
-  return Object.prototype.toString.call(o) === '[object Object]'
-}
-
-function hasChildren(obj){
-  return isArray(obj.children) && obj.children.length > 0
-}
-
-function isChildren(obj){
-  return isArray(obj) && obj.length > 0
-}
-
-function inArray(obj, objList){
-  return objList.indexOf(obj) !== -1
-}
-
-function idxArray(val, key, objList){
-  for (var i=0; i<objList.length; i++){
-    if (objList[i][key] === val){
-      return i
-    }
-  }
-  return -1
-}
-
-function deepClone(obj) {
-  if (obj === undefined){
-    return undefined
-  }
-  return JSON.parse(JSON.stringify(obj))
-}
-
-
-// 简化createElement嵌套写法
-class VVNode{
-  constructor(tag, props={}, children = []){
-    this.tag = tag
-    this.props = props
-    this.children = children
-  }
-  push(vnode) {
-    if (isArray(vnode)){
-      this.children.push(...vnode)
-    }
-    else {
-      if (vnode){
-        this.children.push(vnode)
-      }
-    }
-    
-    return this
-  }
-  resolve(h) {
-    var children = this.children.map(child=>{
-      if (child instanceof VVNode){
-        return child.resolve(h)
-      }
-      else {
-        return child
-      }
-    })
-    return h(this.tag, this.props, children)
-  }
-}
-function hx(tag, props={}, children=[]){
+export function hx(tag, props={}, children=[]){
   if (tag.indexOf('.') !== -1){
     var [realTag, className] = tag.split('.')
     tag = realTag
@@ -82,17 +15,5 @@ function hx(tag, props={}, children=[]){
       })
     }
   }
-
   return new VVNode(tag, props, children)
-}
-
-export {
-  isArray,
-  isObject,
-  hasChildren,
-  isChildren,
-  hx,
-  inArray,
-  idxArray,
-  deepClone
 }
